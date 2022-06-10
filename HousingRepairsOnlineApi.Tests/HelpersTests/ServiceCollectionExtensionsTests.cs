1482ce321948509864b318ba6bc45f904466d7c6
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-06-10 16:19:40 +0100

    Extracted configuration file logic from registering SOR engine with DI

--- HousingRepairsOnlineApi.Tests/HelpersTests/ServiceCollectionExtensionsTests.cs
+++ HousingRepairsOnlineApi.Tests/HelpersTests/ServiceCollectionExtensionsTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Abstractions.TestingHelpers;
 using FluentAssertions;
 using HousingRepairsOnlineApi.Helpers;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +12,7 @@ namespace HousingRepairsOnlineApi.Tests.HelpersTests
 {
     public class ServiceCollectionExtensionsTests
     {
-        private readonly MockFileSystem fileSystem = new();
+        private readonly Mock<ISorConfigurationProvider> sorConfigurationProviderMock = new();
 
         [Fact]
         public void GivenValidSorConfigPathArgument_WhenAddingSoREngineToServices_ThenSoREngineIsRegistered()
@@ -42,11 +40,10 @@ namespace HousingRepairsOnlineApi.Tests.HelpersTests
                     }
                 }
             }";
-            const string configFilePath = "SoRConfig.json";
-            fileSystem.AddFile(configFilePath, new MockFileData(jsonConfig));
+            sorConfigurationProviderMock.Setup(x => x.ConfigurationValue()).Returns(jsonConfig);
 
             // Act
-            ServiceCollectionExtensions.AddSoREngine(serviceCollection, configFilePath, fileSystem);
+            ServiceCollectionExtensions.AddSoREngine(serviceCollection, sorConfigurationProviderMock.Object);
 
             // Assert
             serviceCollectionMock.VerifyAll();
@@ -62,11 +59,10 @@ namespace HousingRepairsOnlineApi.Tests.HelpersTests
             var serviceCollectionMock = new Mock<IServiceCollection>();
             var serviceCollection = serviceCollectionMock.Object;
 
-            const string configFilePath = "SoRConfig.json";
-            fileSystem.AddFile(configFilePath, new MockFileData(configJson));
+            sorConfigurationProviderMock.Setup(x => x.ConfigurationValue()).Returns(configJson);
 
             // Act
-            Action act = () => ServiceCollectionExtensions.AddSoREngine(serviceCollection, configFilePath, fileSystem);
+            Action act = () => ServiceCollectionExtensions.AddSoREngine(serviceCollection, sorConfigurationProviderMock.Object);
 
             // Assert
             act.Should().Throw<InvalidOperationException>().WithInnerException<T>();
@@ -95,44 +91,6 @@ namespace HousingRepairsOnlineApi.Tests.HelpersTests
                 }"
             };
 
-        }
-
-        [Fact]
-        public void GivenPathToMissingSorConfigPathArgument_WhenAddingSoREngineToServices_ThenInvalidOperationExceptionIsThrown()
-        {
-            // Arrange
-            var serviceCollectionMock = new Mock<IServiceCollection>();
-            var serviceCollection = serviceCollectionMock.Object;
-
-            // Act
-            Action act = () => ServiceCollectionExtensions.AddSoREngine(serviceCollection, "SoRConfig.missing.json", fileSystem);
-
-            // Assert
-            act.Should().Throw<InvalidOperationException>().WithInnerException<FileNotFoundException>();
-        }
-
-        [Theory]
-        [MemberData(nameof(InvalidArgumentTestData))]
-#pragma warning disable xUnit1026
-        public void GivenInvalidSorConfigPathArgument_WhenAddingSoREngineToServices_ThenExceptionIsThrown<T>(T exception, string sorConfigPath) where T : Exception
-#pragma warning restore xUnit1026
-        {
-            // Arrange
-            var serviceCollectionMock = new Mock<IServiceCollection>();
-            var serviceCollection = serviceCollectionMock.Object;
-
-            // Act
-            Action act = () => ServiceCollectionExtensions.AddSoREngine(serviceCollection, sorConfigPath);
-
-            // Assert
-            act.Should().Throw<T>();
-        }
-
-        public static IEnumerable<object[]> InvalidArgumentTestData()
-        {
-            yield return new object[] { new ArgumentNullException(), null };
-            yield return new object[] { new ArgumentException(), "" };
-            yield return new object[] { new ArgumentException(), " " };
         }
     }
 }
