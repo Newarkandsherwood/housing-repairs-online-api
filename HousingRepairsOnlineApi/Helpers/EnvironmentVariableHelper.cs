1e1bd8cacfe0a81d4df27074c67f862f47624b9a
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-06-10 17:12:33 +0100

    Extracted environment variable helper

---
+++ HousingRepairsOnlineApi/Helpers/EnvironmentVariableHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HousingRepairsOnlineApi.Helpers
+{
+    public static class EnvironmentVariableHelper
+    {
+        public static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name) ??
+                   throw new InvalidOperationException($"Incorrect configuration: '{name}' environment variable must be set");
+        }
+    }
+}
