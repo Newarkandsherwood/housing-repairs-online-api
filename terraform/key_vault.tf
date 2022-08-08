275c075104667b39bc0988ec987c681595ea549e
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-08-08 17:37:29 +0100

    Add changes to key vault config

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -1,18 +1,17 @@
-resource "azurerm_key_vault" "hro-api-key-vault" {
-  name                        = "${var.service_name}-key-vault"
-  location                    = var.resource_group_location
-  resource_group_name         = var.resource_group_name
-  enabled_for_disk_encryption = true
-  tenant_id                   = var.azure_ad_tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name                    = "standard"
+resource "azurerm_key_vault" "hro-repairs-api-key-vault" {
+  name                       = "hro-repairs-api-key-vault"
+  location                   = var.resource_group_location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = var.azure_ad_tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  sku_name                   = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "hro-api-key-vault-access-policy" {
-  key_vault_id = azurerm_key_vault.hro-api-key-vault.id
+resource "azurerm_key_vault_access_policy" "hro-repairs-api-key-vault-access-policy" {
+  key_vault_id = sensitive(azurerm_key_vault.hro-repairs-api-key-vault.id)
   tenant_id    = var.azure_ad_tenant_id
-  object_id    = var.service_principal_id
+  object_id    = sensitive(azurerm_user_assigned_identity.hro-repairs-api-vault-access-identity.principal_id)
 
   secret_permissions = [
     "Get",
