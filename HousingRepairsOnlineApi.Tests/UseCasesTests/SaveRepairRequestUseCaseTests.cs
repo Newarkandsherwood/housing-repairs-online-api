df344a1abe73e971262018e1b404b258a5f78277
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-10-17 15:13:30 +0100

    Update result of triage mapping to include priority

--- HousingRepairsOnlineApi.Tests/UseCasesTests/SaveRepairRequestUseCaseTests.cs
+++ HousingRepairsOnlineApi.Tests/UseCasesTests/SaveRepairRequestUseCaseTests.cs
@@ -33,6 +33,7 @@ namespace HousingRepairsOnlineApi.Tests.UseCasesTests
             const string Problem = "cupboards";
             const string Issue = "doorHangingOff";
             const string RepairCode = "N373049";
+            var repairTriageDetails = new RepairTriageDetails { ScheduleOfRateCode = RepairCode };
             const string Base64Img = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAABhWlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw1AUhU9TpaJVB4uIOGSoThZERRy1CkWoEGqFVh1MXvojNGlIUlwcBdeCgz+LVQcXZ10dXAVB8AfE0clJ0UVKvC8ptIjxwuN9nHfP4b37AKFWYprVNgZoum2mEnExk10RQ68IIIQe9KNLZpYxK0lJ+NbXPXVT3cV4ln/fn9Wt5iwGBETiGWaYNvE68dSmbXDeJ46woqwSnxOPmnRB4keuKx6/cS64LPDMiJlOzRFHiMVCCystzIqmRjxJHFU1nfKFjMcq5y3OWqnCGvfkLwzn9OUlrtMaQgILWIQEEQoq2EAJNmK066RYSNF53Mc/6Polcink2gAjxzzK0CC7fvA/+D1bKz8x7iWF40D7i+N8DAOhXaBedZzvY8epnwDBZ+BKb/rLNWD6k/RqU4seAb3bwMV1U1P2gMsdYODJkE3ZlYK0hHweeD+jb8oCfbdA56o3t8Y5Th+ANM0qeQMcHAIjBcpe83l3R+vc/u1pzO8H+I9yds6VEEcAAAAJcEhZcwAALiMAAC4jAXilP3YAAAAHdElNRQfmAQcOFjXsyx/IAAAAGXRFWHRDb21tZW50AENyZWF0ZWQgd2l0aCBHSU1QV4EOFwAAAAxJREFUCNdj0HiTBAACtgF3wqeo5gAAAABJRU5ErkJggg==";
             const string FileExtension = "png";
             const string ImgUrl = "http://img.png";
@@ -60,7 +61,7 @@ namespace HousingRepairsOnlineApi.Tests.UseCasesTests
             };
 
             mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
-                .Returns(RepairCode);
+                .Returns(repairTriageDetails);
 
             mockAzureStorageGateway.Setup(x => x.UploadBlob(Base64Img, FileExtension))
                 .ReturnsAsync(ImgUrl);
@@ -82,6 +83,7 @@ namespace HousingRepairsOnlineApi.Tests.UseCasesTests
             const string Problem = "cupboards";
             const string Issue = "doorHangingOff";
             const string RepairCode = "N373049";
+            var repairTriageDetails = new RepairTriageDetails { ScheduleOfRateCode = RepairCode };
 
             var repairRequest = new RepairRequest()
             {
@@ -105,7 +107,7 @@ namespace HousingRepairsOnlineApi.Tests.UseCasesTests
             };
 
             mockSorEngine.Setup(x => x.MapSorCode(Location, Problem, Issue))
-                .Returns(RepairCode);
+                .Returns(repairTriageDetails);
 
             mockCosmosGateway.Setup(x => x.AddRepair(It.IsAny<Repair>()))
                 .ReturnsAsync((Repair r) => r);
