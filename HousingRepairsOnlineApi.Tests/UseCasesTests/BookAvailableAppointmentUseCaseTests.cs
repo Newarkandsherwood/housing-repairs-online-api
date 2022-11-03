156294d87aaaa56180e41adb5999eaaa12e5a076
Mark Warriner <mark.warriner@madetech.com>
2022-11-03 12:39:34 +0000

    Add BookAvailableAppointmentUseCase

---
+++ HousingRepairsOnlineApi.Tests/UseCasesTests/BookAvailableAppointmentUseCaseTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using HousingRepairsOnlineApi.Gateways;
+using HousingRepairsOnlineApi.UseCases;
+using Moq;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.UseCasesTests
+{
+    public class BookAvailableAppointmentUseCaseTests
+    {
+        private const string BookingReference = "bookingReference";
+        private const string SorCode = "sorCode";
+        private const string Priority = "priority";
+        private const string LocationId = "locationId";
+
+        private Mock<IAppointmentsGateway> appointmentsGatewayMock;
+        private readonly BookAvailableAppointmentUseCase systemUnderTest;
+
+        public BookAvailableAppointmentUseCaseTests()
+        {
+            appointmentsGatewayMock = new Mock<IAppointmentsGateway>();
+            systemUnderTest = new BookAvailableAppointmentUseCase(appointmentsGatewayMock.Object);
+        }
+
+        [Fact]
+        public async void GivenAnEmptyBookingReference_WhenExecuting_ThenExceptionIsThrow()
+        {
+            // Arrange
+            var bookingRef = "";
+
+            // Act
+            Func<Task> act = async () =>
+            {
+                await systemUnderTest.Execute(
+                    bookingRef, SorCode, Priority, LocationId, "description"
+                );
+            };
+
+            // Assert
+            await act.Should().ThrowExactlyAsync<ArgumentException>();
+        }
+    }
+}
---
+++ HousingRepairsOnlineApi/UseCases/BookAvailableAppointmentUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using HousingRepairsOnlineApi.Gateways;
+
+namespace HousingRepairsOnlineApi.UseCases
+{
+    public class BookAvailableAppointmentUseCase : IBookAvailableAppointmentUseCase
+    {
+        private readonly IAppointmentsGateway appointmentsGateway;
+
+        public BookAvailableAppointmentUseCase(IAppointmentsGateway appointmentsGateway)
+        {
+            this.appointmentsGateway = appointmentsGateway;
+        }
+
+        public async Task Execute(string bookingReference, string sorCode, string priority, string locationId, string repairDescriptionText)
+        {
+            Guard.Against.NullOrWhiteSpace(bookingReference, nameof(sorCode));
+            Guard.Against.NullOrWhiteSpace(sorCode, nameof(sorCode));
+            Guard.Against.NullOrWhiteSpace(priority, nameof(priority));
+            Guard.Against.NullOrWhiteSpace(locationId, nameof(locationId));
+            Guard.Against.NullOrWhiteSpace(repairDescriptionText, nameof(repairDescriptionText));
+
+            var appointments = await appointmentsGateway.GetAvailableAppointments(sorCode, priority, locationId, null, null);
+            // Need to get next available appointment
+            var appointment = appointments.FirstOrDefault();
+            await appointmentsGateway.BookAppointment(bookingReference, sorCode, priority, locationId, appointment.Date.Value, appointment.Date.Value, repairDescriptionText);
+        }
+    }
+}
