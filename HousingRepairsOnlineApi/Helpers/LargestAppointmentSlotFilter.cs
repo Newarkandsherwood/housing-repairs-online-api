aa817233e2b3ece20e1c2822df2299a9e50324b0
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-03 15:38:34 +0000

    Create appointment slot filter

---
+++ HousingRepairsOnlineApi/Helpers/LargestAppointmentSlotFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HousingRepairsOnlineApi.Domain;
+
+namespace HousingRepairsOnlineApi.Helpers;
+
+public class LargestAppointmentSlotFilter : IAppointmentSlotsFilter
+{
+    private readonly IEnumerable<AppointmentSlotTimeSpan> appointmentSlots;
+
+    public LargestAppointmentSlotFilter(IEnumerable<AppointmentSlotTimeSpan> appointmentSlots)
+    {
+        this.appointmentSlots = appointmentSlots;
+    }
+
+    public IEnumerable<AppointmentSlotTimeSpan> Filter()
+    {
+        return appointmentSlots.Count() <= 1 ? appointmentSlots : FindLargestAppointmentSlot(appointmentSlots);
+    }
+
+    private IEnumerable<AppointmentSlotTimeSpan> FindLargestAppointmentSlot(IEnumerable<AppointmentSlotTimeSpan> appointmentSlotTimeSpans)
+    {
+        var result = appointmentSlotTimeSpans.OrderByDescending(x => x.EndTime - x.StartTime).Take(1);
+
+        return result;
+    }
+}
