e3d8bec064f4ac48db88110f3c2aa273b947ed36
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-21 14:31:56 +0000

    Remove unused use case

--- HousingRepairsOnlineApi/UseCases/IBookAvailableAppointmentUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace HousingRepairsOnlineApi.UseCases
-{
-    public interface IBookAvailableAppointmentUseCase
-    {
-        Task Execute(string bookingReference, string sorCode, string priority, string locationId, string repairDescriptionText);
-    }
-}
