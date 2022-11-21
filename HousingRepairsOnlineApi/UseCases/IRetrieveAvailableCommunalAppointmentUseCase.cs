6463c5eac867de6f5fc26449946f4f4a7468e0fd
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-11-21 12:05:38 +0000

    Create use case to retrieve communal appointments

---
+++ HousingRepairsOnlineApi/UseCases/IRetrieveAvailableCommunalAppointmentUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using HousingRepairsOnlineApi.Domain;
+
+namespace HousingRepairsOnlineApi.UseCases;
+
+public interface IRetrieveAvailableCommunalAppointmentUseCase
+{
+    public Task<AppointmentTime> Execute(string repairLocation, string repairProblem, string repairIssue,
+        string locationId);
+}
---
+++ HousingRepairsOnlineApi/UseCases/RetrieveAvailableCommunalAppointmentUseCase.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using HousingRepairsOnlineApi.Domain;
+using HousingRepairsOnlineApi.Helpers;
+
+namespace HousingRepairsOnlineApi.UseCases;
+
+public class RetrieveAvailableCommunalAppointmentUseCase : IRetrieveAvailableCommunalAppointmentUseCase
+{
+    private readonly IRetrieveAvailableAppointmentsUseCase retrieveAvailableAppointmentsUseCase;
+
+    public RetrieveAvailableCommunalAppointmentUseCase(IRetrieveAvailableAppointmentsUseCase retrieveAvailableAppointmentsUseCase)
+    {
+        Guard.Against.Null(retrieveAvailableAppointmentsUseCase, nameof(retrieveAvailableAppointmentsUseCase));
+
+        this.retrieveAvailableAppointmentsUseCase = retrieveAvailableAppointmentsUseCase;
+    }
+
+    public async Task<AppointmentTime> Execute(string repairLocation, string repairProblem, string repairIssue, string locationId)
+    {
+        Guard.Against.NullOrWhiteSpace(repairLocation, nameof(repairLocation));
+        Guard.Against.NullOrWhiteSpace(repairProblem, nameof(repairProblem));
+        Guard.Against.NullOrWhiteSpace(locationId, nameof(locationId));
+
+        var appointments = await retrieveAvailableAppointmentsUseCase.Execute(RepairType.Communal,
+            repairLocation, repairProblem,
+            repairIssue, locationId);
+
+        var result = appointments.MinBy(x => x.StartTime);
+
+        return result;
+    }
+}
