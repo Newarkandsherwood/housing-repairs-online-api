d3740359f4d8cabef61619b825791c4df1daef1d
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-12-01 16:14:13 +0000

    Extract common query clause

--- HousingRepairsOnlineApi/Helpers/RepairQueryHelper.cs
+++ HousingRepairsOnlineApi/Helpers/RepairQueryHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using HousingRepairsOnlineApi.Domain;
 using Microsoft.Azure.Cosmos.Linq;
 
@@ -12,15 +13,17 @@ namespace HousingRepairsOnlineApi.Helpers
     {
         private readonly ContainerResponse cosmosContainer;
 
+        private readonly Expression<Func<Repair, bool>> isFutureRepair = x => x.Time.StartDateTime > DateTime.Today.Date;
+
         public RepairQueryHelper(ContainerResponse container) => cosmosContainer = container;
 
         public FeedIterator<Repair> GetRepairSearchIterator(string repairType, string propertyReference)
         {
-            var query = cosmosContainer.Container.GetItemLinqQueryable<Repair>().Where(x =>
-                x.RepairType.ToUpper() == repairType.ToUpper()
-                && x.Address.LocationId == propertyReference
-                && x.Time.StartDateTime > DateTime.Today.Date
-            );
+            var query = cosmosContainer.Container.GetItemLinqQueryable<Repair>()
+                .Where(x =>
+                    x.RepairType.ToUpper() == repairType.ToUpper()
+                    && x.Address.LocationId == propertyReference)
+                .Where(isFutureRepair);
 
             var result = query.ToFeedIterator();
 
@@ -32,11 +35,10 @@ namespace HousingRepairsOnlineApi.Helpers
             var repairTypesUppercase = repairTypes.Select(x => x.ToUpperInvariant());
 
             var query = cosmosContainer.Container.GetItemLinqQueryable<Repair>().Where(x =>
-                x.Id.ToUpper() == repairId.ToUpper()
-                && x.Postcode.Replace(" ", "").ToUpper() == postcode.Replace(" ", "").ToUpper()
-                && repairTypesUppercase.Contains(x.RepairType.ToUpper())
-                && x.Time.StartDateTime > DateTime.Today.Date
-            );
+                    x.Id.ToUpper() == repairId.ToUpper()
+                    && x.Postcode.Replace(" ", "").ToUpper() == postcode.Replace(" ", "").ToUpper()
+                    && repairTypesUppercase.Contains(x.RepairType.ToUpper()))
+                .Where(isFutureRepair);
 
             var result = query.ToFeedIterator();
 
