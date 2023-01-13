bfe1e316f010a730d9d8efbd5ee2957030e4b071
Bhavesh Patel <bhavesh.patel@madetech.com>
2023-01-13 11:25:31 +0000

    Add extension method to combine repair description request texts
    This is identical to the equivalent extension method for RepairDescription and could be consolidated in future

---
+++ HousingRepairsOnlineApi.Tests/HelpersTests/RepairDescriptionRequestExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HousingRepairsOnlineApi.Domain;
+using HousingRepairsOnlineApi.Helpers;
+using Xunit;
+
+namespace HousingRepairsOnlineApi.Tests.HelpersTests;
+
+public class RepairDescriptionRequestExtensionsTests
+{
+    [Fact]
+    public void GivenALocationText_WhenAppendLocationDescription_ThenAppendedTextIsReturned()
+    {
+        var repairDescription = new RepairDescriptionRequest { Text = "Text", LocationText = "LocationText" };
+        var result = repairDescription.CombinedDescriptionTexts();
+        result.Should().Be("LocationText Text");
+    }
+    [Fact]
+    public void GivenNoLocationText_WhenAppendLocationDescription_ThenTextIsReturned()
+    {
+        var repairDescription = new RepairDescriptionRequest { Text = "Text" };
+        var result = repairDescription.CombinedDescriptionTexts();
+        result.Should().Be("Text");
+    }
+}
---
+++ HousingRepairsOnlineApi/Helpers/RepairDescriptionRequestExtensions.cs
@@ -0,0 +1,16 @@
+﻿using HousingRepairsOnlineApi.Domain;
+
+namespace HousingRepairsOnlineApi.Helpers;
+
+public static class RepairDescriptionRequestExtensions
+{
+    public static string CombinedDescriptionTexts(this RepairDescriptionRequest repairDescription)
+    {
+        if (repairDescription.LocationText != null)
+        {
+            return repairDescription.LocationText + " " + repairDescription.Text;
+        }
+
+        return repairDescription.Text;
+    }
+}
